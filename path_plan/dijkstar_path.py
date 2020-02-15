3ee8f69d4f6168fd04ddede4a6bfea7126f548bf
Bin Yu <binyu17812@gmail.com>
2020-02-15 17:57:03 +0800

    feature: Dijkstar path plan

---
+++ path_plan/dijkstar_path.py
@@ -0,0 +1,33 @@
+from dijkstar import Graph, find_path
+
+
+def cost_func(u, v, edge, prev_edge):
+    length, name = edge
+    if prev_edge:
+     prev_name = prev_edge[1]
+    else:
+     prev_name = None
+    cost = length
+    if name != prev_name:
+     cost += 10
+    return cost
+
+
+if __name__ == "__main__":
+    #
+    graph = Graph()
+    graph.add_edge(1, 2, 110)
+    graph.add_edge(2, 3, 125)
+    graph.add_edge(3, 4, 108)
+
+    print(find_path(graph, 1, 4))
+
+    # 
+    graph = Graph()
+    graph.add_edge(1, 2, (110, 'Main Street'))
+    graph.add_edge(2, 3, (125, 'Main Street'))
+    graph.add_edge(3, 4, (108, '1st Street'))
+
+    print(find_path(graph, 1, 4, cost_func=cost_func))
+
+

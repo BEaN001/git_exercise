eb3ff569e5f9dffc9fb2a050284eaad8c659cfc7
Bin Yu <binyu17812@gmail.com>
2020-03-15 19:51:53 +0800

    s

--- py_opencv/5_video/read_local_video.py
+++ py_opencv/5_video/read_local_video.py
@@ -7,22 +7,31 @@ cap = cv.VideoCapture(0)
 
 # print(cv.VideoCapture.get(0))
 
-width = 224
-height = 224
+width = 64
+height = 128
 dim = (width, height)
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'MJPG')
+out = cv.VideoWriter('./output.avi', fourcc, 20.0, dim)
 
 while cap.isOpened():
     ret, frame = cap.read()
-    print(ret)
+    print(ret, frame.shape)
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     resize_gray = cv.resize(gray, dim, interpolation=cv.INTER_AREA)
 
     cv.imshow('frame', resize_gray)
+
+    resize_ori = cv.resize(frame, dim, interpolation=cv.INTER_AREA)
+    out.write(resize_ori)
+
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
+out.release()
 cv.destroyAllWindows()
 
 
---
+++ py_opencv/5_video/save_video.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+import cv2
+import warnings
+import numpy as np
+warnings.simplefilter("always")
+
+
+class VideoWriter:
+    def __init__(self, name, width, height, fps=25):
+        # type: (str, int, int, int) -> None
+        if not name.endswith('.avi'):  # 保证文件名的后缀是.avi
+            name += '.avi'
+            warnings.warn('video name should ends with ".avi"')
+        self.__name = name          # 文件名
+        self.__height = height      # 高
+        self.__width = width        # 宽
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # 如果是avi视频，编码需要为MJPG
+        self.__writer = cv2.VideoWriter(name, fourcc, fps, (width, height))
+
+    def write(self, frame):
+        if frame.dtype != np.uint8:  # 检查frame的类型
+            raise ValueError('frame.dtype should be np.uint8')
+        # 检查frame的大小
+        row, col, _ = frame.shape
+        if row != self.__height or col != self.__width:
+            warnings.warn('长和宽不等于创建视频写入时的设置，此frame不会被写入视频')
+            return
+        self.__writer.write(frame)
+
+    def close(self):
+        self.__writer.release()
+
+
+def main():
+    width = 128
+    height = 128
+    vw = VideoWriter('./test.avi', width, height)
+    for i in range(25*50):
+        # 随机生成一幅图像
+        frame = np.random.randint(0, 255, (height, width, 3), dtype=np.uint8)
+        # 写入图像
+        vw.write(frame)
+    # 关闭
+    vw.close()
+
+
+if __name__ == '__main__':
+    main()

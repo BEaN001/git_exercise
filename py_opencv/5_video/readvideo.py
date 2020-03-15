97f78d786a91e2ba36bea75ebe752682812a21d0
Bin Yu <binyu17812@gmail.com>
2020-03-15 17:24:41 +0800

    f

--- py_opencv/5_video/readvideo.py
+++ py_opencv/5_video/readvideo.py
@@ -5,15 +5,20 @@ cap = cv.VideoCapture(0)
 
 # print(cv.VideoCapture.get(0))
 
+width = 224
+height = 224
+dim = (width, height)
+
 while cap.isOpened():
     ret, frame = cap.read()
     print(ret)
-    if ret:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        cv.imshow('frame', gray)
-        if cv.waitKey(0) & 0xFF == ord('q'):
-            break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    resize_gray = cv.resize(gray, dim, interpolation=cv.INTER_AREA)
+
+    cv.imshow('frame', resize_gray)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv.destroyAllWindows()

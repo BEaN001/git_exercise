0523f747ff01bd91e6b8b28edc7a77532a499a4d
Bin Yu <binyu17812@gmail.com>
2020-09-29 15:09:48 +0800

    fa

---
+++ python_tips/sparse_dense_matric.py
@@ -0,0 +1,19 @@
+# dense to sparse
+from numpy import array
+from scipy.sparse import csr_matrix
+# create dense matrix
+A = array([[1, 0, 0, 1, 0, 0], [0, 0, 2, 0, 0, 1], [0, 0, 0, 2, 0, 0]])
+print(A)
+# convert to sparse matrix (CSR method)
+S = csr_matrix(A)
+print(S)
+print(S.data)
+print(S.indptr)
+print(S.indices)
+# reconstruct dense matrix
+B = S.todense()
+print(B)
+
+
+
+
